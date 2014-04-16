63ec10e2ef666277162b216376e8bfbaa8052917
Romero Dias <romero.dias@hotmail.com>
2014-04-16 17:06:53 -0300

    First Update
    It's the first update of the project

---
+++ src/br/com/rdtecnologia/relatorio/ClienteREL.java
@@ -0,0 +1,56 @@
+package br.com.rdtecnologia.relatorio;
+
+import java.util.List;
+
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import br.com.rdtecnologia.model.Cliente;
+
+public class ClienteREL 
+{
+	/**
+	 * Caminho base
+	 */
+	private String path;
+	
+	/**
+	 * Caminho para o package onde estão armazenados os relatorios Jarper
+	 */
+	private String pathToReportPackage;
+	
+	/**
+	 * Recupera os caminhos para que a classe possa encontrar os relatórios
+	 */
+	public ClienteREL() {
+		this.path = this.getClass().getClassLoader().getResource("").getPath();
+		this.pathToReportPackage = this.path + "br/com/rdtecnologia/jasper/";
+		System.out.println(path);
+	}
+	
+	public String getPathToReportPackage() {
+		return this.pathToReportPackage;
+	}
+	
+	public String getPath() {
+		return this.path;
+	}	
+
+	
+	/**
+	 * Imprime/gera uma lista de Clientes
+	 * @param clientes
+	 */
+	public void imprimir(List<Cliente> clientes) throws Exception	
+	{
+		   
+		JasperReport report = JasperCompileManager.compileReport(this.getPathToReportPackage() + "Clientes.jrxml");
+		
+		JasperPrint print = JasperFillManager.fillReport(report, null, new JRBeanCollectionDataSource(clientes));
+
+		JasperExportManager.exportReportToPdfFile(print, "c:/Relatorio_de_Clientes.pdf");		
+	}
+}
